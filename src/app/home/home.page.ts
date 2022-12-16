852d5238f8f42594844913c203881f239d47378e
JJMUNPER <jmunper007@g.educaand.es>
2022-12-16 12:54:13 +0100

    Suba para terminar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,7 +12,8 @@ export class HomePage {
   users: any;
 
   constructor(private httpClient: HttpClient) {
-  
+
+    this.users = this.httpClient.get('https://randomuser.me/api?results=20')  
   }
 
 }
